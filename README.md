7eb34d6d02a34d698d6684e3fab63d2ea19976fc
edumt <edumoura1997@gmail.com>
2021-07-15 12:39:47 -0300

    finishing excel functionality

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-## BVK Energia Solar - Partial budgeting automation (in Python)
+## BVK Energia Solar - Budgeting automation (in Python)
 
-Script to generate (some/simple) PV budgeting reports
+Script to generate (some/simple) photovoltaic budgeting reports
 
 #### TO DO:
 - [x] ~~Make states dictionary~~
 - [ ] Define classes and respective attributes/functions (class diagram)
 - [x] ~~Open and edit excel sheet~~
-- [ ] Complete (basic) excel functionality
-- [ ] Paste from excel to word
-- [ ] Generate complete word report
-- [ ] GUI (basic)
+- [ ] Finish excel functionality
+- [ ] Generate word report
+- [ ] Basic GUI
--- script.py
+++ script.py
@@ -1,15 +1,19 @@
 # https://automatetheboringstuff.com/chapter12/
-# TO DO: organize script into multiple files
 
-import os
-import datetime
-import openpyxl
+#import os
+#import datetime
+#from openpyxl import Workbook
+from openpyxl import load_workbook
+from docx import Document
 
 CONST_SHEET_VERSION = '13-JULHO-2021'
 CONST_CONSUMPTION_CELL = 'D18'
 CONST_NAME_CELL = 'C4'
 CONST_PERCENTAGE_INCREASE_CELL = 'H44'
-CONST_BUDGET_CELL = 'B3:H32'
+CONST_OPTION_1_PANEL_NUMBER_CELL = 'D11'
+CONST_OPTION_2_PANEL_NUMBER_CELL = 'F11'
+CONST_OPTION_3_PANEL_NUMBER_CELL = 'H11'
+CONST_BUDGET_CELL = 'B3:H32' 
 
 statesDict = {
     "ES": "ESPÍRITO SANTO",
@@ -19,32 +23,52 @@ statesDict = {
 }
 
 class Client:
-    def __init__(self, fullName, consumption, state, city = ''):
-        self.fullName = fullName.upper()
+    def __init__(self, name, consumption, state, city = ''):
+        self.name = name.upper()
         self.consumption = consumption # Average monthly energy consumption [kWh/month]
         self.state = state.upper() # State initials, e.g. ES
         self.city = city.upper() # (District-)City, e.g. PRAIA DE ITAPARICA-VILA VELHA
     
-def populateSheet(client):
-    # TO DO: maybe dinamically change number of PV panels
-    wb = openpyxl.load_workbook('../excel_template.xlsx')
-    #print(wb.sheetnames)
-    wb['HCONSUMO'][CONST_CONSUMPTION_CELL].value = client.consumption
-    return wb
+    def getFormulaSheet(self):
+        self.sheet = load_workbook('../excel_template.xlsx')
+        
+    def getDataSheet(self):
+        self.sheet = load_workbook('../excel_template.xlsx', data_only=True)
+    
+    def populateSheet(self):
+        #print(sheet.sheetnames)
+        self.sheet['HCONSUMO'][CONST_CONSUMPTION_CELL].value = self.consumption
+        self.sheet['HCONSUMO'][CONST_NAME_CELL].value = ('CLIENTE: {name}').format(name=self.name)
+        self.setPanelsQuantity()
+        
+    def setPanelsQuantity(self):
+        # TO DO: dinamically change number of PV panels
+        self.sheet['Preço SFCR-GROWATT PHONO 450Wp'][CONST_OPTION_2_PANEL_NUMBER_CELL].value =\
+        (self.sheet['Preço SFCR-GROWATT PHONO 450Wp'][CONST_OPTION_1_PANEL_NUMBER_CELL].value + '+1')
+            
+        self.sheet['Preço SFCR-GROWATT PHONO 450Wp'][CONST_OPTION_3_PANEL_NUMBER_CELL].value =\
+        (self.sheet['Preço SFCR-GROWATT PHONO 450Wp'][CONST_OPTION_2_PANEL_NUMBER_CELL].value + '+1')
 
-def copyBudgetArea(sheet):
-    pass
+    def saveSheet(self):
+        # TO DO: learn mkdir() and/or save() best practices, what to do when dir/file aready exists, exception handling etc
+        #os.mkdir(('../{name}').format(name=client.name))
+        #self.sheet.save(('../{name}/GERADORES-{name}-ALDO-{version}.xlsx').format(name=self.name, version=CONST_SHEET_VERSION))
+        self.sheet.save('../test.xlsx')
+        #pass
+    
+    def generateSheet(self):
+        self.getFormulaSheet()
+        self.populateSheet()
+        self.saveSheet()
+        print('Success! Sheet generated.')
 
-def saveSheet(sheet, client):
-    # TO DO: learn mkdir() and save() best practices, what to do when dir/file aready exists, exception handling etc
-    #wb.save('../test.xlsx')
-    os.mkdir(('../{name}').format(name=client.fullName))
-    sheet.save(('../{name}/GERADORES-{name}-ALDO-{version}-{time}.xlsx').format(name=client.fullName, version=CONST_SHEET_VERSION, time=datetime.time()))
+    def copyBudgetArea(self):
+        self.getDataSheet()
 
-def generateReport(client, sheet):
-    pass
+    def generateReport(self):
+        pass
 
-test_client = Client('Eduardo Moura Tavares', 500, 'ES', 'Praia de Itaparica-Vila Velha')
+# TO DO: CLI and, eventually, a GUI
+test_client = Client('Eduardo Moura Tavares', 5000, 'ES', 'Praia de Itaparica-Vila Velha')
 #print(vars(test_client))
-wb = populateSheet(test_client)
-print(wb['HCONSUMO'][CONST_CONSUMPTION_CELL].value)
+test_client.generateSheet()
