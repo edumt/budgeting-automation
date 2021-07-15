f16a6c6c3f232d9943c5dbcf4cdb564b2b71872f
edumt <edumoura1997@gmail.com>
2021-07-14 21:02:36 -0300

    defined some consts

--- script.py
+++ script.py
@@ -20,16 +20,21 @@ class Client:
 def populateSheet(client):
     print(client.name)
 
-def generateReport():
+def generateReport(client, sheet):
     pass
+
+CONST_CONSUMPTION_CELL = 'D18'
+CONST_NAME_CELL = 'C4'
+CONST_PERCENTAGE_INCREASE_CELL = 'H44'
+CONST_BUDGET_CELL = 'B3:H32'
 
 test_client = Client('Eduardo Moura Tavares', 500, 'ES', 'Praia de Itaparica-Vila Velha')
 #print(vars(test_client))
 populateSheet(test_client)
 
-wb = openpyxl.load_workbook('excel_template.xlsx')
+wb = openpyxl.load_workbook('../excel_template.xlsx')
 #print(wb.sheetnames)
 
 consumption_sheet = wb['HCONSUMO']
 #print(consumption_sheet)
-#print(consumption_sheet['D18'].value)
+print(consumption_sheet[CONST_CONSUMPTION_CELL].value)
