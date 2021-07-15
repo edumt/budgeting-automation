df71b5f9ecfab0d2eb3df66be52a87478693f2d7
edumt <edumoura1997@gmail.com>
2021-07-14 22:00:50 -0300

    added TO DO comments

--- script.py
+++ script.py
@@ -22,21 +22,21 @@ class Client:
     def __init__(self, fullName, consumption, state, city = ''):
         self.fullName = fullName.upper()
         self.consumption = consumption # Average monthly energy consumption [kWh/month]
-        self.state = state.upper() # State initials
-        self.city = city.upper() # (District-)City
+        self.state = state.upper() # State initials, e.g. ES
+        self.city = city.upper() # (District-)City, e.g. PRAIA DE ITAPARICA-VILA VELHA
     
 def populateSheet(client):
+    # TO DO: maybe dinamically change number of PV panels
     wb = openpyxl.load_workbook('../excel_template.xlsx')
     #print(wb.sheetnames)
-    consumption_sheet = wb['HCONSUMO']
-    consumption_sheet[CONST_CONSUMPTION_CELL].value = client.consumption
-    print(consumption_sheet[CONST_CONSUMPTION_CELL].value)
+    wb['HCONSUMO'][CONST_CONSUMPTION_CELL].value = client.consumption
     return wb
 
 def copyBudgetArea(sheet):
     pass
 
 def saveSheet(sheet, client):
+    # TO DO: learn mkdir() and save() best practices, what to do when dir/file aready exists, exception handling etc
     #wb.save('../test.xlsx')
     os.mkdir(('../{name}').format(name=client.fullName))
     sheet.save(('../{name}/GERADORES-{name}-ALDO-{version}-{time}.xlsx').format(name=client.fullName, version=CONST_SHEET_VERSION, time=datetime.time()))
@@ -47,4 +47,4 @@ def generateReport(client, sheet):
 test_client = Client('Eduardo Moura Tavares', 500, 'ES', 'Praia de Itaparica-Vila Velha')
 #print(vars(test_client))
 wb = populateSheet(test_client)
-print(wb)
+print(wb['HCONSUMO'][CONST_CONSUMPTION_CELL].value)
