694ce2f507bd3a1bd5d0140dcbc36b4c2c659080
edumt <edumoura1997@gmail.com>
2021-07-14 21:50:49 -0300

    functions update

--- script.py
+++ script.py
@@ -1,7 +1,15 @@
 # https://automatetheboringstuff.com/chapter12/
 # TO DO: organize script into multiple files
 
+import os
+import datetime
 import openpyxl
+
+CONST_SHEET_VERSION = '13-JULHO-2021'
+CONST_CONSUMPTION_CELL = 'D18'
+CONST_NAME_CELL = 'C4'
+CONST_PERCENTAGE_INCREASE_CELL = 'H44'
+CONST_BUDGET_CELL = 'B3:H32'
 
 statesDict = {
     "ES": "ESPÍRITO SANTO",
@@ -11,30 +19,32 @@ statesDict = {
 }
 
 class Client:
-    def __init__(self, fullName, aConsumption, state, city = ''):
-        self.name = fullName.upper()
-        self.aConsumption = aConsumption # Average monthly energy consumption [kWh/month]
+    def __init__(self, fullName, consumption, state, city = ''):
+        self.fullName = fullName.upper()
+        self.consumption = consumption # Average monthly energy consumption [kWh/month]
         self.state = state.upper() # State initials
         self.city = city.upper() # (District-)City
     
 def populateSheet(client):
-    print(client.name)
+    wb = openpyxl.load_workbook('../excel_template.xlsx')
+    #print(wb.sheetnames)
+    consumption_sheet = wb['HCONSUMO']
+    consumption_sheet[CONST_CONSUMPTION_CELL].value = client.consumption
+    print(consumption_sheet[CONST_CONSUMPTION_CELL].value)
+    return wb
+
+def copyBudgetArea(sheet):
+    pass
+
+def saveSheet(sheet, client):
+    #wb.save('../test.xlsx')
+    os.mkdir(('../{name}').format(name=client.fullName))
+    sheet.save(('../{name}/GERADORES-{name}-ALDO-{version}-{time}.xlsx').format(name=client.fullName, version=CONST_SHEET_VERSION, time=datetime.time()))
 
 def generateReport(client, sheet):
     pass
 
-CONST_CONSUMPTION_CELL = 'D18'
-CONST_NAME_CELL = 'C4'
-CONST_PERCENTAGE_INCREASE_CELL = 'H44'
-CONST_BUDGET_CELL = 'B3:H32'
-
 test_client = Client('Eduardo Moura Tavares', 500, 'ES', 'Praia de Itaparica-Vila Velha')
 #print(vars(test_client))
-populateSheet(test_client)
-
-wb = openpyxl.load_workbook('../excel_template.xlsx')
-#print(wb.sheetnames)
-
-consumption_sheet = wb['HCONSUMO']
-#print(consumption_sheet)
-print(consumption_sheet[CONST_CONSUMPTION_CELL].value)
+wb = populateSheet(test_client)
+print(wb)
